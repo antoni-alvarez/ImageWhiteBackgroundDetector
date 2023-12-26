91598aba22d24c07b77c42d7f827cf15f8e24176
Antoni Alvarez <t.alvarezpascual@gmail.com>
2023-12-26 13:01:23 +0100

    Analyze background for each border separately

--- app/src/Application/UseCase/ImageAnalyze/ValidBackground.php
+++ app/src/Application/UseCase/ImageAnalyze/ValidBackground.php
@@ -9,6 +9,7 @@ use GdImage;
 use Random\Randomizer;
 use Symfony\Component\Filesystem\Exception\IOException;
 
+use function abs;
 use function file_get_contents;
 use function imagecolorat;
 use function imagecolorsforindex;
@@ -20,24 +21,20 @@ use function sprintf;
 
 class ValidBackground
 {
-    private const int BORDER_TOP = 1;
-    private const int BORDER_BOTTOM = 2;
-    private const int BORDER_LEFT = 3;
-    private const int BORDER_RIGHT = 4;
-
     private const int WHITE_VALUE = 255;
     private const int WHITE_LIKE_MIN_VALUE = 230;
     private const int ALPHA_VALUE = 127;
 
-    private const int NUM_POINTS = 10000;
-    private const float MIN_VALID_PERCENTAGE = 0.6;
+    private const int NUM_POINTS = 2500;
+    private const float MIN_BORDER_VALID_PERCENTAGE = 0.6;
+    private const float MIN_SINGLE_BORDER_VALID_PERCENTAGE = 0.3;
 
     /**
      * Google recommendation: 0.1 to 0.25
      * "Frame your product in the image space so that it takes up no less than 75%, but not more than 90%, of the full image.".
      */
     private const float BORDER_SIZE = 0.01;
-    private const int MAX_DISTANCE = 5;
+    private const int MAX_DISTANCE = 8;
 
     private bool $strictMode = false;
 
@@ -60,25 +57,99 @@ class ValidBackground
         $borderWidth = max(1, (int) ($width * self::BORDER_SIZE));
         $borderHeight = max(1, (int) ($height * self::BORDER_SIZE));
 
-        $whitePoints = 0;
-        $transparentPoints = 0;
+        $topWhitePoints = 0;
+        $rightWhitePoints = 0;
+        $bottomWhitePoints = 0;
+        $leftWhitePoints = 0;
+
+        $topTransparentPoints = 0;
+        $rightTransparentPoints = 0;
+        $bottomTransparentPoints = 0;
+        $leftTransparentPoints = 0;
 
         for ($i = 0; $i < self::NUM_POINTS; $i++) {
-            $pixelColor = $this->getRandomBorderPixelColor($width, $borderHeight, $height, $borderWidth, $image);
+            $pixelColor = $this->getTopBorderRandomPixelColor($width, $borderWidth, $borderHeight, $image);
 
             if ($this->isWhitePixel($pixelColor)) {
-                $whitePoints++;
+                $topWhitePoints++;
             }
 
             if ($this->isTransparentPixel($pixelColor)) {
-                $transparentPoints++;
+                $topTransparentPoints++;
             }
         }
 
-        $hasWhiteBackground = $whitePoints / self::NUM_POINTS >= self::MIN_VALID_PERCENTAGE;
-        $hasTransparentBackground = $transparentPoints / self::NUM_POINTS >= self::MIN_VALID_PERCENTAGE;
+        $topWhitePercentage = $topWhitePoints / self::NUM_POINTS;
+        $topTransparentPercentage = $topTransparentPoints / self::NUM_POINTS;
 
-        return $hasWhiteBackground  || $hasTransparentBackground;
+        if ($topWhitePercentage < self::MIN_SINGLE_BORDER_VALID_PERCENTAGE && $topTransparentPercentage < self::MIN_SINGLE_BORDER_VALID_PERCENTAGE) {
+            return false;
+        }
+
+        for ($i = 0; $i < self::NUM_POINTS; $i++) {
+            $pixelColor = $this->getRightBorderRandomPixelColor($width, $height, $borderWidth, $image);
+
+            if ($this->isWhitePixel($pixelColor)) {
+                $rightWhitePoints++;
+            }
+
+            if ($this->isTransparentPixel($pixelColor)) {
+                $rightTransparentPoints++;
+            }
+        }
+
+        $rightWhitePercentage = $rightWhitePoints / self::NUM_POINTS;
+        $rightTransparentPercentage = $rightTransparentPoints / self::NUM_POINTS;
+
+        if ($rightWhitePercentage < self::MIN_SINGLE_BORDER_VALID_PERCENTAGE && $rightTransparentPercentage < self::MIN_SINGLE_BORDER_VALID_PERCENTAGE) {
+            return false;
+        }
+
+        for ($i = 0; $i < self::NUM_POINTS; $i++) {
+            $pixelColor = $this->getBottomBorderRandomPixelColor($width, $height, $borderWidth, $borderHeight, $image);
+
+            if ($this->isWhitePixel($pixelColor)) {
+                $bottomWhitePoints++;
+            }
+
+            if ($this->isTransparentPixel($pixelColor)) {
+                $bottomTransparentPoints++;
+            }
+        }
+
+        $bottomWhitePercentage = $bottomWhitePoints / self::NUM_POINTS;
+        $bottomTransparentPercentage = $bottomTransparentPoints / self::NUM_POINTS;
+
+        if ($bottomWhitePercentage < self::MIN_SINGLE_BORDER_VALID_PERCENTAGE && $bottomTransparentPercentage < self::MIN_SINGLE_BORDER_VALID_PERCENTAGE) {
+            return false;
+        }
+
+        for ($i = 0; $i < self::NUM_POINTS; $i++) {
+            $pixelColor = $this->getLeftBorderRandomPixelColor($height, $borderWidth, $image);
+
+            if ($this->isWhitePixel($pixelColor)) {
+                $leftWhitePoints++;
+            }
+
+            if ($this->isTransparentPixel($pixelColor)) {
+                $leftTransparentPoints++;
+            }
+        }
+
+        $leftWhitePercentage = $leftWhitePoints / self::NUM_POINTS;
+        $leftTransparentPercentage = $leftTransparentPoints / self::NUM_POINTS;
+
+        if ($leftWhitePercentage < self::MIN_SINGLE_BORDER_VALID_PERCENTAGE && $leftTransparentPercentage < self::MIN_SINGLE_BORDER_VALID_PERCENTAGE) {
+            return false;
+        }
+
+        $sumWhitePercentage = $topWhitePercentage + $rightWhitePercentage + $bottomWhitePercentage + $leftWhitePercentage;
+        $sumTransparentPercentage = $topTransparentPercentage + $rightTransparentPercentage + $bottomTransparentPercentage + $leftTransparentPercentage;
+
+        $meanWhitePercentage = $sumWhitePercentage / 4;
+        $meanTransparentPercentage = $sumTransparentPercentage / 4;
+
+        return $meanWhitePercentage > self::MIN_BORDER_VALID_PERCENTAGE || $meanTransparentPercentage > self::MIN_BORDER_VALID_PERCENTAGE;
     }
 
     public function setStrictMode(bool $strictMode): void
@@ -87,35 +158,80 @@ class ValidBackground
     }
 
     /**
+     * @param array<string, int> $rgb
+     */
+    public function getMeanColorDistance(array $rgb): float
+    {
+        $distanceRB = abs($rgb['red'] - $rgb['blue']);
+        $distanceGR = abs($rgb['green'] - $rgb['red']);
+        $distanceBG = abs($rgb['blue'] - $rgb['green']);
+
+        return ($distanceRB + $distanceGR + $distanceBG) / 3;
+    }
+
+    /**
      * @return array<string, int>
      */
-    private function getRandomBorderPixelColor(int $width, int $borderHeight, int $height, int $borderWidth, GdImage $image): array
+    private function getTopBorderRandomPixelColor(int $width, int $borderWidth, int $borderHeight, GdImage $image): array
     {
-        $border = $this->randomizer->getInt(self::BORDER_TOP, self::BORDER_RIGHT);
-
-        switch ($border) {
-            case self::BORDER_TOP:
-                $x = $this->randomizer->getInt(0, $width - 1);
-                $y = $this->randomizer->getInt(0, $borderHeight - 1);
-                break;
-            case self::BORDER_BOTTOM:
-                $x = $this->randomizer->getInt(0, $width - 1);
-                $y = $this->randomizer->getInt($height - $borderHeight, $height - 1);
-                break;
-            case self::BORDER_LEFT:
-                $x = $this->randomizer->getInt(0, $borderWidth - 1);
-                $y = $this->randomizer->getInt(0, $height - 1);
-                break;
-            case self::BORDER_RIGHT:
-            default:
-                $x = $this->randomizer->getInt($width - $borderWidth, $width - 1);
-                $y = $this->randomizer->getInt(0, $height - 1);
-        }
+        $x = $this->randomizer->getInt($borderWidth, $width - $borderWidth);
+        $y = $this->randomizer->getInt(0, $borderHeight - 1);
 
         $color = imagecolorat($image, $x, $y);
 
         if ($color === false) {
-            throw new IOException(sprintf('Error reading color info at %s:%s', $x, $y));
+            throw new IOException(sprintf('Error reading color info at top border %s:%s', $x, $y));
+        }
+
+        return imagecolorsforindex($image, $color);
+    }
+
+    /**
+     * @return array<string, int>
+     */
+    private function getRightBorderRandomPixelColor(int $width, int $height, int $borderWidth, GdImage $image): array
+    {
+        $x = $this->randomizer->getInt($width - $borderWidth, $width - 1);
+        $y = $this->randomizer->getInt(0, $height - 1);
+
+        $color = imagecolorat($image, $x, $y);
+
+        if ($color === false) {
+            throw new IOException(sprintf('Error reading color info at right border %s:%s', $x, $y));
+        }
+
+        return imagecolorsforindex($image, $color);
+    }
+
+    /**
+     * @return array<string, int>
+     */
+    private function getBottomBorderRandomPixelColor(int $width, int $height, int $borderWidth, int $borderHeight, GdImage $image): array
+    {
+        $x = $this->randomizer->getInt($borderWidth, $width - $borderWidth);
+        $y = $this->randomizer->getInt($height - $borderHeight, $height - 1);
+
+        $color = imagecolorat($image, $x, $y);
+
+        if ($color === false) {
+            throw new IOException(sprintf('Error reading color info at bottom border %s:%s', $x, $y));
+        }
+
+        return imagecolorsforindex($image, $color);
+    }
+
+    /**
+     * @return array<string, int>
+     */
+    private function getLeftBorderRandomPixelColor(int $height, int $borderWidth, GdImage $image): array
+    {
+        $x = $this->randomizer->getInt(0, $borderWidth - 1);
+        $y = $this->randomizer->getInt(0, $height - 1);
+
+        $color = imagecolorat($image, $x, $y);
+
+        if ($color === false) {
+            throw new IOException(sprintf('Error reading color info at left border %s:%s', $x, $y));
         }
 
         return imagecolorsforindex($image, $color);
@@ -154,25 +270,13 @@ class ValidBackground
             return true;
         }
 
-        if ($rgb['red'] < self::WHITE_LIKE_MIN_VALUE ||
-            $rgb['green'] < self::WHITE_LIKE_MIN_VALUE ||
-            $rgb['blue'] < self::WHITE_LIKE_MIN_VALUE) {
+        if ($rgb['red'] < self::WHITE_LIKE_MIN_VALUE
+            || $rgb['green'] < self::WHITE_LIKE_MIN_VALUE
+            || $rgb['blue'] < self::WHITE_LIKE_MIN_VALUE) {
             return false;
         }
 
         return $this->getMeanColorDistance($rgb) < self::MAX_DISTANCE;
-    }
-
-    /**
-     * @param array<string, int> $rgb
-     */
-    public function getMeanColorDistance(array $rgb): float
-    {
-        $distanceRB = abs($rgb['red'] - $rgb['blue']);
-        $distanceGR = abs($rgb['green'] - $rgb['red']);
-        $distanceBG = abs($rgb['blue'] - $rgb['green']);
-
-        return ($distanceRB + $distanceGR + $distanceBG) / 3;
     }
 
     private function loadImage(string $imagePath): GdImage
