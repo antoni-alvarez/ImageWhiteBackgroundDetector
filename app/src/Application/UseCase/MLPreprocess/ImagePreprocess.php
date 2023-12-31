59a79a9ea954ea796c23a32d90ade1975ebeb882
Antoni Alvarez <t.alvarezpascual@gmail.com>
2023-12-31 03:01:14 +0100

    Fix standards

--- app/src/Application/UseCase/MLPreprocess/ImagePreprocess.php
+++ app/src/Application/UseCase/MLPreprocess/ImagePreprocess.php
@@ -24,7 +24,7 @@ use const PATHINFO_FILENAME;
 
 class ImagePreprocess
 {
-    const int IMAGE_SIZE = 32;
+    private const int IMAGE_SIZE = 32;
 
     public function __construct(
         private readonly Filesystem $filesystem,
